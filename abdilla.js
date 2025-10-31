ad83c67edab183c03417aa723a3d22f3fb3e2e42
Dhabith1195 <budianakbaik05@gmail.com>
2025-10-31 13:18:49 +0700

    Add files via upload

---
+++ abdilla.js
@@ -0,0 +1,25 @@
+function nama() {
+    alert("SELAMAT");
+} nama();
+
+function saya(perkenalan) {
+    return perkenalan;
+} document.write(`assalamualaikum, perkenalkan nama saya Dhabith Abdillah Adipraja`)
+
+function kelas(kls) {
+    return kls;
+} document.write(` saya kelas X/PPLG `)
+
+function sekul(sekolah) {
+    return sekolah;
+} document.write(`saya sekolah di SMK TI Bazma `)
+
+function alamat(alamat) {
+    return alamat;
+} document.write(`rumah saya di bojonggede kab. bogor `)
+
+function hobi(hobiii) {
+    return hobiii;
+} document.write(`hobi saya makan`)
+
+
